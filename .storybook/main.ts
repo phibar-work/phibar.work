93e7536f1a0b7c0cb73a19df0f67427be4a81b0e
Philipp Bartels <me@phibar.work>
2026-01-28 14:15:16 +0000

    linting

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,11 +2,7 @@ import type { StorybookConfig } from '@storybook/nextjs-vite'
 
 const config: StorybookConfig = {
   stories: ['../src/components/design-system/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-vitest',
-    '@storybook/addon-a11y',
-    '@storybook/addon-docs',
-  ],
+  addons: ['@storybook/addon-vitest', '@storybook/addon-a11y', '@storybook/addon-docs'],
   framework: '@storybook/nextjs-vite',
   staticDirs: ['../public'],
 }
